8842e7ea70acb70d229106b8a953d81c7e40fec3
TyaaaAbimanyu <adit.manyu@gmail.com>
2024-11-08 08:07:56 +0700

    test data

--- app/Config/Firebase.php
+++ app/Config/Firebase.php
@@ -14,8 +14,8 @@ class Firebase extends BaseConfig
     'measurementId' => getenv('FIREBASE_MEASUREMENT_ID')
   ];
 
-  public $databaseUrl = null;
-  public $credentials = null;
+  public $databaseUrl = 1;
+  public $credentials = 2;
 
   public function __construct()
   {
