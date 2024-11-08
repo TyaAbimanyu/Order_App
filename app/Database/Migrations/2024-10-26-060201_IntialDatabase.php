90961908019444297854a8eeec9007d06b2c8517
TyaaaAbimanyu <adit.manyu@gmail.com>
2024-11-08 08:02:59 +0700

    test data

--- app/Database/Migrations/2024-10-26-060201_IntialDatabase.php
+++ app/Database/Migrations/2024-10-26-060201_IntialDatabase.php
@@ -2,17 +2,35 @@
 
 namespace App\Database\Migrations;
 
+use App\Libraries\FirebaseService;
 use CodeIgniter\Database\Migration;
+use Ramsey\Uuid\Uuid;
 
 class IntialDatabase extends Migration
 {
-    public function up()
-    {
-        //
-    }
+  protected $firebaseService;
+  public function __construct()
+  {
+      $this->firebaseService = new FirebaseService();
+  }
+  public function up()
+  {
+    $usersCollection = $this->firebaseService->firestore->collection('users');
+    $usersCollection->document('sampleUser')->set([
+      'user_id' => 1,
+      'user_uuid' => Uuid::uuid4()->toString(),
+      'user_name' => 'Sample User',
+      'user_email' => 'sampleuser@example.com',
+      'user_password' => 123456,
+      'user_active' => true,
+    ]);
+  }
 
-    public function down()
-    {
-        //
-    }
+  public function down()
+  {
+    $usersCollection = $this->firebaseService->firestore->collection('users');
+    $usersCollection->document('sampleUser')->delete();
+
+    echo "Firestore initial document deleted.";
+  }
 }
